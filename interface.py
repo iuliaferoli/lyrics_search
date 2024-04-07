bc36361cf08cd653dfbad07db0ac5ae00ec70561
Iulia Feroli <iuliaferoli@gmail.com>
2024-04-07 14:59:54 +0200

    added pydantic model validation and a quick test with the json file

--- interface.py
+++ interface.py
@@ -1,31 +1,52 @@
-from fastapi import FastAPI
-import json
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
-with open('hozier_songs.json', 'r') as f:
-  songs = json.load(f)
+import json
 
 app = FastAPI()
 
+class Song(BaseModel):
+    name: str
+    artist: str
+    lyrics: str | None
+    id : int
 
-@app.get("/")
-def read_root():
-    return "Welcome to the demo"
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Take me to church",
+                    "artist": "Hozier",
+                    "lyrics": "Her eyes and words are so icy\nOh but she burns\nLike rum on the fire",
+                    "id": 0,
+                }
+            ]
+        }
+    }
 
 
-@app.get("/songs/{song_id}")
-def read_item(song_id: int):
-    # get song with this id
-    # display song & lyrics
-    return {}
+
+@app.get("/songs/{id}")
+def read_root(id:int):
+    # get song based on Id
+    # display song
+    # for testing - we just use the json file for now
+    with open('hozier_songs.json', 'r') as f:
+        songs = json.load(f)
+    song = Song.model_validate(songs[id])
+    return song
 
 
 @app.get("/search/{query}")
 def search_item(query: str):
     # get list of songs based on query
     # display songs & lyrics
-    return {songs[0]["artist"]: songs[0]["name"]}
+    return {}
 
 
 @app.put("/songs/{song_id}")
-def save_item(song_id: int, song: dict):
+def save_item(song_id: int, song: Song):
+    #add song to index
     return {"Song added to the index"}
